6e88d65b23acf8d4dafa2ce21848235276ed88c0
Roshan <kroshan197@gmail.com>
2024-12-26 18:57:02 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Hero from './components/Hero.jsx'
+
+const App = () => {
+    return (
+        <main class="relative min-h-screen w-screen overflow-x-hidden">
+            <Hero />
+        </main>
+    )
+}
+export default App
